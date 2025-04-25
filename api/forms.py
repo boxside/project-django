95f532ff2415ae2fc51e71cd7f5364ef496c323f
Muhammad Fikri Fadillah <m.fikrfadillah27@gmail.com>
2025-04-25 14:36:56 +0700

    Drop files from .gitignore

--- api/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Parameter
-
-class ParameterForm(forms.ModelForm):
-    class Meta:
-        model = Parameter
-        fields = ['amount']  # Hanya 'amount' yang bisa diubah
--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Parameter
-
-class ParameterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Parameter
-        fields = ['id','id_param','title','amount','updated_at']
-
-    
